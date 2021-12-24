3153349e628a5fa8219fde9121217b353b28ffa1
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-12-24 09:51:03 +0300

    Подключение к базе данных

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 
 const express = require('express');
-
+const sequlize = require('./db');
 const PORT = process.env.PORT || PORT;
 
 const app = express();
 
-app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequlize.authenticate();
+    await sequlize.sync();
+    app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
