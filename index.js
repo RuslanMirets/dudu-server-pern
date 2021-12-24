b6df7c33954f5a2aeb1a0744e45e82e528fec152
Ruslan Mirets <ruslan.mirets@gmail.com>
2021-12-24 10:25:46 +0300

    Роутинг и эндпоинты юзера

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 require('dotenv').config();
-
 const express = require('express');
-const sequlize = require('./db');
-const PORT = process.env.PORT || PORT;
+const sequelize = require('./db');
+const models = require('./models/models');
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+const router = require('./routes/index');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
   try {
-    await sequlize.authenticate();
-    await sequlize.sync();
-    app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
